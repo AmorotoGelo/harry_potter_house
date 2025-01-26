f9ea5a09a9eee18384bcbed888eaee083fa4d0eb
Carl Angelo Amoroto <carlamoroto200526@gmail.com>
2025-01-26 21:37:53 +0800

    Set condition for movie genre, each specific words were added to list of houses

--- harry_potter_house.py
+++ harry_potter_house.py
@@ -12,4 +12,16 @@ while True:
         print(" MUGGLE! You are not on the masterlist")
         break
     else:
-        movie_genre = input("What movie genre you like?: ")    
+        movie_genre = input("What movie genre you like?: ")
+
+        if movie_genre == "action":
+            gryffindor.append(input_name)
+        elif movie_genre == "science fiction":
+            ravenclaw.append(input_name)
+        elif movie_genre == "horror":
+            slytherin.append(input_name)
+        elif movie_genre == "drama":
+            hufflepuff.append(input_name)    
+        else:
+            print("Choose among horror, action, science fiction, and drama only")  
+
