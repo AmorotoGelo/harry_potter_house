7429ca0df5cae477a688c940fe3d07a06ac9feca
Carl Angelo Amoroto <carlamoroto200526@gmail.com>
2025-01-28 22:11:02 +0800

    Loop each question to ask again the same question once invalid input has been received

--- harry_potter_house.py
+++ harry_potter_house.py
@@ -6,9 +6,14 @@ hufflepuff = 0
 while True:
 
     input_name = input("What is your name?: ")
-    if not input_name.endswith("a") or input_name.endswith("A"):
-        print("Welcome! You may proceed: ")
-        
+    if input_name.endswith("a") or input_name.endswith("A"):
+        print("MUGGLE! YOU ARE NOT WELCOME!!!")
+        break
+
+    print("WELCOME! YOU MAY START YOUR SORTING JOURNEY")
+
+    while True:
+
         movie_genre = input("What is your favorite movie genre?: ")
 
         if movie_genre == "action":
@@ -18,9 +23,12 @@ while True:
         elif movie_genre == "horror":
             slytherin += 1
         elif movie_genre == "drama":
-            hufflepuff += 1   
+            hufflepuff += 1  
+            break 
         else:
             print("Choose among horror, action, science fiction, and drama only!")  
+
+    while True:
 
         animal = input("Which animal would you like to be a pet?: ")
 
@@ -32,9 +40,12 @@ while True:
             slytherin += 1
         elif animal == "rat":
             hufflepuff += 1
+            break
         else:
             print("Choose among cat, owl, toad, or cat!")
-      
+
+    while True:
+          
         personality = input("Are you shy, confident, friendly, or arrogant?: ")
 
         if personality == "confident":
@@ -45,11 +56,8 @@ while True:
             slytherin += 1
         elif personality == "friendly":
             hufflepuff += 1
+            break
         else:
             print("Choose among the given choices!")   
     
-    else:
-        print("MUGGLE! YOU ARE NOT WELCOME!!!")
-        break
-
-   
+  
